5e3e47fc7ed02e421275bf49a2f8fddcbf80c1d6
JESUS TELMO NENE <neneter45@gmail.com>
2023-05-10 08:06:58 -0500

    Version-1.0.10

---
+++ BookOnline-v2/src/main/java/com/uplink/BookOnlinev2/BookOnlinev2Application.java
@@ -0,0 +1,13 @@
+package com.uplink.BookOnlinev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookOnlinev2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookOnlinev2Application.class, args);
+	}
+
+}
---
+++ BookOnline-v2/src/test/java/com/uplink/BookOnlinev2/BookOnlinev2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uplink.BookOnlinev2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookOnlinev2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
